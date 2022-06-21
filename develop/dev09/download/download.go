f5f1eaeaede6dcb06958ac1a891b548a84b4cf20
Dmitry Shtanko <lempionnewliving@gmail.com>
2022-06-21 17:09:28 +0300

    dev09 done

--- develop/dev09/download/download.go
+++ develop/dev09/download/download.go
@@ -1,46 +1,106 @@
 package download
 
 import (
-	"fmt"
+	"io"
 	"net/http"
+	"os"
 	"strings"
-	"sync"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
-func Store(dest string, url string, wg *sync.WaitGroup) error {
-	defer wg.Done()
-	resp, err := http.Get(url)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	doc, err := goquery.NewDocumentFromReader(resp.Body)
+func Download(dest string, url string) error {
+	fname, err := download(dest, url)
 	if err != nil {
 		return err
 	}
 
-	doc.Find("img").Each(findElement("src"))
-	doc.Find("script").Each(findElement("src"))
-	doc.Find("link").Each(findElement("href"))
+	f, err := os.OpenFile(fname, os.O_RDWR, 0644)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	doc, err := goquery.NewDocumentFromReader(f)
+	if err != nil {
+		return err
+	}
+
+	resources := make([]string, 0)
+
+	doc.Find("img").Each(findElement("src", &resources))
+	doc.Find("script").Each(findElement("src", &resources))
+	doc.Find("link").Each(findElement("rel", &resources))
+
+	for _, resource := range resources {
+		_, err = download(dest, url+resource)
+		if err != nil {
+			return err
+		}
+	}
+
 	return nil
 }
 
-func findElement(attr string) func(i int, selection *goquery.Selection) {
+func download(dest string, url string) (string, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	//dest = addSlash(dest)
+	path := []string{dest}
+	divided := strings.Split(resp.Request.URL.String(), "/")
+	path = append(path, divided[2:]...)
+
+	err = os.MkdirAll(strings.Join(path[:len(path)-1], "/"), 0777)
+
+	var file *os.File
+	if resp.Header.Get("content-type") == "text/html" {
+		file, err = os.Create(strings.Join(path, "/") + "index.html")
+	} else {
+		file, err = os.Create(strings.Join(path, "/"))
+	}
+
+	if err != nil {
+		return "", err
+	}
+
+	defer file.Close()
+
+	_, err = io.Copy(file, resp.Body)
+
+	return file.Name(), nil
+}
+
+func findElement(attr string, output *[]string) func(i int, selection *goquery.Selection) {
 	return func(i int, selection *goquery.Selection) {
 		src, _ := selection.Attr(attr)
+		if src == "stylesheet" {
+			src, _ = selection.Attr("href")
+		}
+
 		// БОЛЬШОЕ СПАСИБО GOQUERY ЗА ВАЛИДАЦИЮ ПО ПУСТОЙ СТРОКЕ А НЕ ПО OK, XD
 		if src == "" {
 			return
 		}
 
 		r := []rune(src)
-		sub := strings.Split(src, ".")
 
-		if r[0] == '/' && r[0] != r[1] && sub[len(sub)-1] != "html" {
-			fmt.Println(src)
+		if r[0] == '/' && r[0] != r[1] {
+			*output = append(*output, src)
+			selection.SetAttr(attr, "."+src)
 		}
 	}
 }
+
+//func addSlash(dest string) string {
+//	runes := []rune(dest)
+//	if runes[len(runes)-1] != '/' {
+//		runes = append(runes, '/')
+//	}
+//
+//	dest = string(runes)
+//	return dest
+//}
--- develop/dev09/task.go
+++ develop/dev09/task.go
@@ -4,31 +4,23 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"sync"
 
 	"dev09/download"
 )
-
-var wg sync.WaitGroup
 
 func main() {
 	destination := flag.String("d", "./tmp", "destination for output")
 
 	flag.Parse()
 
-	urls := flag.Args()
-	if len(urls) < 1 {
-		fmt.Println("usage: task [option] [URL]...")
+	args := flag.Args()
+	if len(args) != 1 {
+		fmt.Println("usage: task [option] [URL]")
 		return
 	}
 
-	for _, v := range urls {
-		wg.Add(1)
-		err := download.Store(*destination, v, &wg)
-		if err != nil {
-			log.Fatal(err)
-		}
+	err := download.Download(*destination, args[0])
+	if err != nil {
+		log.Fatal(err)
 	}
-
-	wg.Wait()
 }
